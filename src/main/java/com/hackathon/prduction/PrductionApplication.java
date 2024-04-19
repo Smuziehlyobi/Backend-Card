50f3331d7ed9c048358b431fefbcb9eaa8263fc3
Andrey Nachevny <nochergt@gmail.com>
2024-04-19 20:01:32 +0300

    init project

---
+++ src/main/java/com/hackathon/prduction/PrductionApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.prduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/prduction/PrductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.prduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
