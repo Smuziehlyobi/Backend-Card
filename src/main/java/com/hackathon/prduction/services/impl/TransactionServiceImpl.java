7d6de78f0d27cb4ad60fd208bd57e1ba4d462f2e
glebushnik <glebushnik@MacBook-Pro-glebushnik.local>
2024-04-20 10:56:19 +0300

    user mapper added

---
+++ src/main/java/com/hackathon/prduction/services/impl/TransactionServiceImpl.java
@@ -0,0 +1,60 @@
+package com.hackathon.prduction.services.impl;
+
+import com.hackathon.prduction.domain.dto.TransactionDTO;
+import com.hackathon.prduction.domain.entity.Transaction;
+import com.hackathon.prduction.domain.mapper.TransactionMapper;
+import com.hackathon.prduction.exceptions.transaction.TransactionNotFoundByIdException;
+import com.hackathon.prduction.repository.TransactionRepository;
+import com.hackathon.prduction.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+    private final TransactionService transactionService;
+    private final TransactionRepository transactionRepository;
+    private final TransactionMapper transactionMapper;
+
+    @Autowired
+    public TransactionServiceImpl(TransactionService transactionService, TransactionRepository transactionRepository, TransactionMapper transactionMapper) {
+        this.transactionService = transactionService;
+        this.transactionRepository = transactionRepository;
+        this.transactionMapper = transactionMapper;
+    }
+
+    @Override
+    public TransactionDTO createTransaction(Transaction transaction) {
+        TransactionDTO transactionDTO = transactionMapper.toDto(transaction);
+        transactionRepository.save(transaction);
+        return transactionDTO;
+    }
+
+    @Override
+    public void deleteTransaction(Long transactionId) throws TransactionNotFoundByIdException {
+        Transaction transaction = transactionRepository.findById(transactionId).orElse(null);
+        if(transaction == null){
+            throw new TransactionNotFoundByIdException("Transaction with such ID does not exist.");
+        }
+        transactionRepository.delete(transaction);
+    }
+
+    @Override
+    public List<TransactionDTO> getAllTransactions() {
+        List<Transaction> transactions = transactionRepository.findAll();
+        List<TransactionDTO> transactionDTOs = transactionMapper.toDto(transactions);
+        return transactionDTOs;
+    }
+
+    @Override
+    public TransactionDTO getOneTransaction(Long transactionId) throws TransactionNotFoundByIdException {
+        Transaction transaction = transactionRepository.findById(transactionId).orElse(null);
+        if(transaction == null) {
+            throw new TransactionNotFoundByIdException("Transaction with such ID does not exist.");
+        }
+        TransactionDTO transactionDTO = transactionMapper.toDto(transaction);
+        return transactionDTO;
+    }
+
+}
