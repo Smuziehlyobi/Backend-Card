6f32c8afd4358258993347fb78ea0de69ca9b544
Alexander-Karpenko <0greengrass0@gmail.com>
2024-04-20 17:12:39 +0300

    fixSecurity

--- src/main/java/com/hackathon/prduction/security/service/impl/UserServiceImpl.java
+++ src/main/java/com/hackathon/prduction/security/service/impl/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.hackathon.prduction.security.service.impl;
 
 import com.hackathon.prduction.domain.dto.security.CreateUserRequestDTO;
+import com.hackathon.prduction.domain.entity.Role;
 import com.hackathon.prduction.domain.entity.User;
 import com.hackathon.prduction.domain.mapper.CreateUserRequestDTOMapper;
+import com.hackathon.prduction.exceptions.role.RoleNotFoundException;
+import com.hackathon.prduction.repository.RoleRepository;
 import com.hackathon.prduction.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,12 +19,21 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final CreateUserRequestDTOMapper createUserRequestDTOMapper;
+    private final PasswordEncoder encoder;
+
 
 
     @Transactional
     public User save(CreateUserRequestDTO userResponseDTO) {
         User user = createUserRequestDTOMapper.toEntity(userResponseDTO);
+        Role role = roleRepository.findByName("USER").orElse(null);
+        if(role == null){
+            throw new RoleNotFoundException("Role not found");
+        }
+        user.setRole(role);
+        user.setPassword(encoder.encode(userResponseDTO.getPassword()));
         userRepository.save(user);
         return user;
     }
